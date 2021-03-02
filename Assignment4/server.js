cac6a499d3d81aa55623a24133aee6eccd8290c6
anpr9067 <anpr9067@colorado.edu>
2021-03-01 18:35:27 -0700

    Changes in layout

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -3,28 +3,37 @@
     const http = require('http');
     const WebSocketServer = require('websocket').server;
     const server = http.createServer();
-    var requestedhumval = 0;
-    var requestedtempval = 0;
+    var finalresult1;
+    var finalresult2;
     server.listen(9898);
     const wsServer = new WebSocketServer({
         httpServer: server
     });
     wsServer.on('request', function(request) {
-        const numdata = "SELECT count(*) as namecount FROM eid.sensor;";
-        con.query(numdata, function(err, result, fields){
+        const sensordata0 = "SELECT * FROM eid.sensor where sensornumber = 0 ORDER BY id DESC LIMIT 10;";
+        const sensordata1 = "SELECT * FROM eid.sensor where sensornumber = 1 ORDER BY id DESC LIMIT 10;";
+        
+        con.query(sensordata0, function(err, result, fields){
             if (err) throw err;
             console.log("query success");
-            console.log(result);
-            requestedhumval = result[0].namecount;
-            //requestedtempval = result[0].Tempreture;
-            console.log(requestedhumval);
+            //console.log(result);
+            finalresult1 = result;
+            
+        });
+        con.query(sensordata1, function(err, result, fields){
+            if (err) throw err;
+            console.log("query success");
+            //console.log(result);
+            finalresult2 = result;
+            
         });
 
         const connection = request.accept(null, request.origin);
         connection.on('message', function(message) {
+          console.log(finalresult1);
           console.log('Received Message:', message);;
-          console.log(requestedhumval);
-          connection.send(requestedhumval);
+          connection.send(JSON.stringify(finalresult1));
+          connection.send(JSON.stringify(finalresult2));
         });
         connection.on('close', function(reasonCode, description) {
             console.log('Client has disconnected.');

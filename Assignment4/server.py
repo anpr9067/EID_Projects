9b8de9361b8368cb879ab5e73f8a701413d1277b
Nitik Gupta <nigu8044@colorado.edu>
2021-03-05 17:21:18 -0700

    Added request on button functionality

--- Assignment4/server.py
+++ Assignment4/server.py
@@ -39,41 +39,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
             content = {'id': result[0], 'TimeStamp':result[1], 'sensornumber':result[2],"Tempreture":result[3],"Humidity":result[4],"Alarm_Count_Temp":result[5],"Alarm_Count_Hum":result[6],"Error_Count":result[7]}
             data2.append(content)
         data1 = json.dumps(data2)
-        data = [[]]*(sensor_num*2)
-        flag = [0]*(sensor_num*2)
-        base_seconds = 0;
-        buffer = "SELECT * FROM eid.sensor where sensornumber="+str(sensorid)+" ORDER BY id LIMIT 10;"
-        cursor.execute(buffer)
-        for (ID,TIME,SENSOR,TEMP,HUMIDITY,ALARM1,ALARM2,ERROR) in cursor:
-        # Converting to base number of seconds
-            TimeStamp = datetime.strptime(TIME, "%m/%d/%Y, %H:%M:%S %p")
-            seconds = int(TimeStamp.timestamp())
-            if(base_seconds == 0):
-                base_seconds = int(TimeStamp.timestamp())
-            seconds = seconds - base_seconds
-            if flag[2*SENSOR] == 0:
-                if (TEMP != 999):
-                    data[2*SENSOR]=[TEMP]
-                    data[2*SENSOR+1]= [seconds]
-                    flag[2*SENSOR]=1
-            elif flag[2*SENSOR] == 1:
-                if(TEMP!=999):
-                    data[2*SENSOR].append(TEMP)
-                    data[2*SENSOR+1].append(seconds)
-        # Different markers for different plots
-        # Plotting all the curves
-        plt.clf()
-        plt.plot(data[2*sensorid+1],data[2*sensorid])
-        # Plotting all the curves
-        plt.xlabel('Time(in seconds)')
-        plt.ylabel('Temperature(in Fahrenheit)')
-        plt.title('Temperature variations by sensor' + str(sensorid))
-        plt.savefig('Plot.png')
         self.write_message(data1)
-        # print(data)
-        # Reverse Message and send it back
-        # print ('sending back message: %s' % temp_data[1][-1])
-        # self.write_message(json.dumps(temp_data))
  
     def on_close(self):
         print ('connection closed')

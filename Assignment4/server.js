5aa5d6f46abe97003321c750ff2c66a7a3623546
Nitik Gupta <nigu8044@colorado.edu>
2021-03-05 18:47:45 -0700

    Node with one time working

--- Assignment4/server.js
+++ Assignment4/server.js
@@ -2,27 +2,16 @@ const con = require('./db.js');
     const http = require('http');
     const WebSocketServer = require('websocket').server;
     const server = http.createServer();
-    var finalresult1;
-    var finalresult2;
     var sensorid = 0;
     server.listen(9898);
     const wsServer = new WebSocketServer({
         httpServer: server
     });
     wsServer.on('request', function(request) {
-        var sensordata0 = "SELECT * FROM eid.sensor where sensornumber = "+sensorid+" ORDER BY id DESC LIMIT 10;";
-        console.log(sensordata0);
-        con.query(sensordata0, function(err, result, fields){
-            if (err) throw err;
-            console.log("query success");
-            //console.log(result);
-            finalresult1 = result;
-            
-        });
 
         const connection = request.accept(null, request.origin);
         
-        connection.on('message', function(message) {
+        connection.on('message',  function(message) {
 
           console.log('Received Message:', message.utf8Data);
           sensorid = message.utf8Data;
@@ -32,11 +21,9 @@ const con = require('./db.js');
             if (err) throw err;
             console.log("query success");
             //console.log(result);
-            finalresult1 = result;
-            
+            connection.send(JSON.stringify(result));
             });
-          console.log(sensordata0);
-          connection.send(JSON.stringify(finalresult1));
+          
           /*connection.send(JSON.stringify(finalresult1));
           connection.send(JSON.stringify(finalresult2));*/
         });
